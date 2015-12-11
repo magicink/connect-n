17f98038e2a8f47d17f247b3264440d3ff75711d
Brandon Tom <magicink@gmail.com>
2015-12-10 23:38:07 -0800

    Implemented logic for checking horizontal win conditions

--- src/core.js
+++ src/core.js
@@ -24,10 +24,7 @@ export class Column {
     }
     return addedChecker
   }
-  getAvailableRows () {
-    return this.availableRows
-  }
-  reset() {
+  reset () {
     if (this.maxRows > 0) {
       this.availableRows = this.maxRows
       this.rows = []
@@ -43,20 +40,78 @@ export class Column {
 export class Board {
   constructor (x, y, winningLength = 4) {
     this.columns = []
-    this.winningLength = winningLength
+    this.height = 0
+    this.width = 0
+    this.winningLength = (Number.isInteger(winningLength) && winningLength > 0) ? winningLength : 4
+    this.isDiagonalWinPossible = true
+    this.isWinningPossible = true
+    this.isWon = false
     if (Number.isInteger(x) && Number.isInteger(y) && x > 0 && y > 0) {
+      this.height = y
+      this.width = x
+      if (winningLength > x || winningLength > y) {
+        this.isDiagonalWinPossible = false
+      }
+      if (winningLength > x && winningLength > y) {
+        this.isWinningPossible = false
+      }
       while (x > 0) {
         this.columns.push(new Column(y))
         x--
       }
     }
   }
-  isBoardFull () {
+  addChecker (playerId, column) {
+    let addedChecker = false
+    if (
+      !this.isFull() &&
+      Number.isInteger(column) &&
+      this.columns.length > 0 &&
+      column > 0 &&
+      column <= this.columns.length
+    ) {
+      addedChecker = this.columns[column - 1].addChecker(playerId)
+      if (addedChecker && this.isWinningPossible) {
+        this.checkWinningCondition(playerId, column, this.height - this.columns[column - 1].availableRows)
+      }
+    }
+    return addedChecker
+  }
+  checkWinningCondition (playerId, x, y) {
+    let count = 1
+    let maxLeftTravel = Math.min(x - 1, this.winningLength - 1)
+    let maxRightTravel = Math.min(this.width - x, this.winningLength - 1)
+
+    while (maxLeftTravel > 0) {
+      maxLeftTravel--
+      if (this.columns[maxLeftTravel].rows[y - 1] !== playerId || count >= this.winningLength) {
+        break
+      } else {
+        count++
+      }
+    }
+    let rightTravelIndex = x
+    while (maxRightTravel > 0) {
+      maxRightTravel--
+      if (this.columns[rightTravelIndex].rows[y - 1] !== playerId || count >= this.winningLength) {
+        break
+      } else {
+        count++
+      }
+      rightTravelIndex++
+    }
+
+    if (count < this.winningLength) {
+      count = 1
+    }
+  }
+
+  isFull () {
     let full = false
     if (this.columns.length > 0) {
       let availableSlots = 0
       for (let i in this.columns) {
-        availableSlots += this.columns[i].getAvailableRows()
+        availableSlots += this.columns[i].availableRows
       }
       if (availableSlots === 0) {
         full = true
@@ -64,17 +119,7 @@ export class Board {
     }
     return full
   }
-  addChecker (playerId, column) {
-    let addedChecker = false
-    if (
-      !this.isBoardFull() &&
-      Number.isInteger(column) &&
-      this.columns.length > 0 &&
-      column > 0 &&
-      column <= this.columns.length
-    ) {
-      addedChecker = this.columns[column-1].addChecker(playerId)
-    }
-    return addedChecker
+  reset () {
+
   }
 }
--- test/core.spec.js
+++ test/core.spec.js
@@ -25,22 +25,22 @@ describe('Core', () => {
       let column = new Column(3)
       it('should add a checker to the bottom row', () => {
         column.addChecker(1)
-        expect(column.rows).to.deep.equal([1,0,0])
+        expect(column.rows).to.deep.equal([1, 0, 0])
         expect(column.availableRows).to.equal(2)
         column.addChecker(2)
-        expect(column.rows).to.deep.equal([1,2,0])
+        expect(column.rows).to.deep.equal([1, 2, 0])
         expect(column.availableRows).to.equal(1)
       })
       it('should handle invalid input data', () => {
         column.addChecker('A')
         column.addChecker(0.208)
         column.addChecker(-1)
-        expect(column.rows).to.deep.equal([1,2,0])
+        expect(column.rows).to.deep.equal([1, 2, 0])
       })
       it('should not let you add more checkers once all rows are filled', () => {
         column.addChecker(3)
         expect(column.addChecker(4)).to.equal(false)
-        expect(column.rows).to.deep.equal([1,2,3])
+        expect(column.rows).to.deep.equal([1, 2, 3])
         expect(column.availableRows).to.equal(0)
       })
     })
@@ -52,7 +52,7 @@ describe('Core', () => {
       column.addChecker(1)
       it('should clear all checkers from the row', () => {
         column.reset()
-        expect(column.rows).to.deep.equal([0,0,0,0,0])
+        expect(column.rows).to.deep.equal([0, 0, 0, 0, 0])
         expect(column.availableRows).to.equal(5)
       })
     })
@@ -60,36 +60,82 @@ describe('Core', () => {
   describe('Board', () => {
     describe('Construction', () => {
       it('should construct a board with the appropriate number columns', () => {
-        const board = new Board(3,4)
+        const board = new Board(3, 4)
         expect(board).to.deep.equal({
           columns: [
-            { availableRows: 4, maxRows: 4, rows: [0,0,0,0] },
-            { availableRows: 4, maxRows: 4, rows: [0,0,0,0] },
-            { availableRows: 4, maxRows: 4, rows: [0,0,0,0] }
+            { availableRows: 4, maxRows: 4, rows: [0, 0, 0, 0] },
+            { availableRows: 4, maxRows: 4, rows: [0, 0, 0, 0] },
+            { availableRows: 4, maxRows: 4, rows: [0, 0, 0, 0] }
           ],
+          height: 4,
+          isDiagonalWinPossible: false,
+          isWinningPossible: true,
+          isWon: false,
+          width: 3,
           winningLength: 4
         })
       })
     })
     describe('Adding Checker', () => {
-      it('Should properly add checkers to columns', () => {
-        let board = new Board(2,2)
-        board.addChecker(2,1)
+      let board = new Board(2, 2)
+      it('should properly add checkers to columns', () => {
+        board.addChecker(1, 1)
+        board.addChecker(2, 2)
+        board.addChecker(3, 1)
         expect(board).to.deep.equal({
           columns: [
-            {availableRows: 1, maxRows: 2, rows:[2,0]},
-            {availableRows: 2, maxRows: 2, rows:[0,0]}
+            { availableRows: 0, maxRows: 2, rows: [1, 3] },
+            { availableRows: 1, maxRows: 2, rows: [2, 0] }
           ],
+          height: 2,
+          isDiagonalWinPossible: false,
+          isWinningPossible: false,
+          isWon: false,
+          width: 2,
           winningLength: 4
         })
-        board.addChecker(1,1)
+      })
+      it('should detect when the board is full', () => {
+        expect(board.isFull()).to.equal(false)
+        board.addChecker(4, 2)
+        expect(board.isFull()).to.equal(true)
+      })
+      it('should not add more any checkers once the board is full', () => {
+        expect(board.addChecker(5, 1)).to.equal(false)
         expect(board).to.deep.equal({
           columns: [
-            {availableRows: 0, maxRows: 2, rows:[2,1]},
-            {availableRows: 2, maxRows: 2, rows:[0,0]}
+            { availableRows: 0, maxRows: 2, rows: [1, 3] },
+            { availableRows: 0, maxRows: 2, rows: [2, 4] }
           ],
+          height: 2,
+          isDiagonalWinPossible: false,
+          isWinningPossible: false,
+          isWon: false,
+          width: 2,
           winningLength: 4
         })
+      })
+    })
+    describe('Checking Winning Conditions', () => {
+      it('Should detect whether winning is possible', () => {
+        let boardA = new Board(7, 6)
+        expect(boardA.isWinningPossible).to.equal(true)
+        let boardB = new Board(3, 3)
+        expect(boardB.isWinningPossible).to.equal(false)
+      })
+      it('Should detect whether a diagonal win is possible', () => {
+        let boardA = new Board(7, 6)
+        expect(boardA.isDiagonalWinPossible).to.equal(true)
+        let boardB = new Board(7, 2)
+        expect(boardB.isDiagonalWinPossible).to.equal(false)
+        expect(boardB.isWinningPossible).to.equal(true)
+      })
+      it('Should attempt determine if the board has been won if winning is possible', () => {
+        const board = new Board(7, 6)
+        board.addChecker(1, 1)
+        board.addChecker(1, 2)
+        board.addChecker(1, 3)
+        board.addChecker(1, 4)
       })
     })
   })
