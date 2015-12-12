1d424665f5082b7dd84897f99863a8f6e490d01e
Brandon Tom <magicink@gmail.com>
2015-12-12 08:09:52 -0800

    Broke out the unit test for each component

---
+++ test/column.spec.js
@@ -0,0 +1,58 @@
+import Column from '../src/column'
+import { expect } from 'chai'
+
+describe('Column', () => {
+  describe('Construction', () => {
+    it('should create a column of the appropriate size', () => {
+      let column = new Column(5)
+      expect(column.rows.length).to.equal(5)
+      column = new Column(25)
+      expect(column.rows.length).to.equal(25)
+    })
+    it('should handle strings invalid input data', () => {
+      let columnA = new Column('test')
+      expect(columnA.rows.length).to.equal(0)
+
+      let columnB = new Column(-5)
+      expect(columnB.rows.length).to.equal(0)
+
+      let columnC = new Column(0.008)
+      expect(columnC.rows.length).to.equal(0)
+    })
+  })
+  describe('Adding Checker', () => {
+    let column = new Column(3)
+    it('should add a checker to the bottom row', () => {
+      column.addChecker(1)
+      expect(column.rows).to.deep.equal([1, 0, 0])
+      expect(column.availableRows).to.equal(2)
+      column.addChecker(2)
+      expect(column.rows).to.deep.equal([1, 2, 0])
+      expect(column.availableRows).to.equal(1)
+    })
+    it('should handle invalid input data', () => {
+      column.addChecker('A')
+      column.addChecker(0.208)
+      column.addChecker(-1)
+      expect(column.rows).to.deep.equal([1, 2, 0])
+    })
+    it('should not let you add more checkers once all rows are filled', () => {
+      column.addChecker(3)
+      expect(column.addChecker(4)).to.equal(false)
+      expect(column.rows).to.deep.equal([1, 2, 3])
+      expect(column.availableRows).to.equal(0)
+    })
+  })
+  describe('Reset', () => {
+    let column = new Column(5)
+    column.addChecker(1)
+    column.addChecker(1)
+    column.addChecker(2)
+    column.addChecker(1)
+    it('should clear all checkers from the row', () => {
+      column.reset()
+      expect(column.rows).to.deep.equal([0, 0, 0, 0, 0])
+      expect(column.availableRows).to.equal(5)
+    })
+  })
+})
---
+++ test/game.spec.js
@@ -0,0 +1,66 @@
+import Board from '../src/board'
+import { expect } from 'chai'
+import Game, { GAME_STATE_ACTIVE, GAME_STATE_DRAW, GAME_STATE_WON } from '../src/game'
+
+describe('Game', () => {
+  let game = new Game()
+  describe('Construction', () => {
+    it('should construct a game with the default settings', () => {
+      expect(game.board).to.deep.equal(new Board(7, 6))
+      expect(game.players).to.deep.equal([
+        {playerId: 1, name: 'Player 1', wins: 0},
+        {playerId: 2, name: 'Player 2', wins: 0}
+      ])
+      expect(game.totalPlayers).to.equal(2)
+      expect(game.currentPlayer).to.equal(1)
+    })
+  })
+  describe('Game Play', () => {
+    it('should be active', () => {
+      expect(game.gameState).to.equal(GAME_STATE_ACTIVE)
+    })
+    it('should detect a victory', () => {
+      game.addChecker(1)
+      expect(game.currentPlayer).to.equal(2)
+      game.addChecker(2)
+      expect(game.currentPlayer).to.equal(1)
+      game.addChecker(2)
+      game.addChecker(3)
+      game.addChecker(4)
+      game.addChecker(7)
+      game.addChecker(8)
+      expect(game.currentPlayer).to.equal(1)
+      game.addChecker(7)
+      game.addChecker(4)
+      game.addChecker(5)
+      game.addChecker(6)
+      game.addChecker(6)
+      game.addChecker(5)
+      game.addChecker(4)
+      game.addChecker(5)
+      game.addChecker(3)
+      game.addChecker(3)
+      game.addChecker(5)
+      game.addChecker(6)
+      game.addChecker(6)
+      game.addChecker(7)
+      game.addChecker(6)
+      expect(game.gameState).to.equal(GAME_STATE_WON)
+      expect(game.winner).to.deep.equal({
+        playerId: 1,
+        name: 'Player 1',
+        wins: 1
+      })
+    })
+    it('should detect a draw', () => {
+      game.configure(15)
+      expect(game.players.length).to.equal(15)
+      for (let i = 1; i < 16; i++) {
+        for (let j = 1; j < 8; j++) {
+          game.addChecker(j)
+        }
+      }
+      expect(game.gameState).to.equal(GAME_STATE_DRAW)
+    })
+  })
+})
