d973946e8bc10702a03b4d69469a37ad82686fe3
Brandon Tom <magicink@gmail.com>
2015-12-12 15:16:02 -0800

    Experimenting with node-jsdom

--- test/js/game.spec.js
+++ test/js/game.spec.js
@@ -1,35 +1,35 @@
-import Board from '../src/js/board'
-import { expect } from 'chai'
-import Game, { GAME_STATE_ACTIVE, GAME_STATE_DRAW, GAME_STATE_WON } from '../src/js/game'
+import Board from '../../src/js/board'
+import expect from 'expect'
+import Game, { GAME_STATE_ACTIVE, GAME_STATE_DRAW, GAME_STATE_WON } from '../../src/js/game'
 
 describe('Game', () => {
   let game = new Game()
   describe('Construction', () => {
     it('should construct a game with the default settings', () => {
-      expect(game.board).to.deep.equal(new Board(7, 6))
-      expect(game.players).to.deep.equal([
+      expect(game.board).toEqual(new Board(7, 6))
+      expect(game.players).toEqual([
         {playerId: 1, wins: 0},
         {playerId: 2, wins: 0}
       ])
-      expect(game.totalPlayers).to.equal(2)
-      expect(game.currentPlayer).to.equal(1)
+      expect(game.totalPlayers).toBe(2)
+      expect(game.currentPlayer).toBe(1)
     })
   })
   describe('Game Play', () => {
     it('should be active', () => {
-      expect(game.gameState).to.equal(GAME_STATE_ACTIVE)
+      expect(game.gameState).toBe(GAME_STATE_ACTIVE)
     })
     it('should detect a victory', () => {
       game.addChecker(1)
-      expect(game.currentPlayer).to.equal(2)
+      expect(game.currentPlayer).toBe(2)
       game.addChecker(2)
-      expect(game.currentPlayer).to.equal(1)
+      expect(game.currentPlayer).toBe(1)
       game.addChecker(2)
       game.addChecker(3)
       game.addChecker(4)
       game.addChecker(7)
       game.addChecker(8)
-      expect(game.currentPlayer).to.equal(1)
+      expect(game.currentPlayer).toBe(1)
       game.addChecker(7)
       game.addChecker(4)
       game.addChecker(5)
@@ -45,21 +45,21 @@ describe('Game', () => {
       game.addChecker(6)
       game.addChecker(7)
       game.addChecker(6)
-      expect(game.gameState).to.equal(GAME_STATE_WON)
-      expect(game.winner).to.deep.equal({
+      expect(game.gameState).toBe(GAME_STATE_WON)
+      expect(game.winner).toEqual({
         playerId: 1,
         wins: 1
       })
     })
     it('should detect a draw', () => {
       game.configure(15)
-      expect(game.players.length).to.equal(15)
+      expect(game.players.length).toBe(15)
       for (let i = 1; i < 16; i++) {
         for (let j = 1; j < 8; j++) {
           game.addChecker(j)
         }
       }
-      expect(game.gameState).to.equal(GAME_STATE_DRAW)
+      expect(game.gameState).toBe(GAME_STATE_DRAW)
     })
   })
 })
---
+++ test/js/setup.js
@@ -0,0 +1,1 @@
+import jsdom from 'node-jsdom'
