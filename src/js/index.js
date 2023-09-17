4055d8e0b859690f10f1d30e7786dbd2f4a292f2
Brandon Tom <brandon@geocaching.com>
2023-09-17 10:39:39 -0700

    feat: ts conversion

--- src/js/index.js
+++
@@ -1,8 +0,0 @@
-import '../scss/app.scss'
-import GameUI from './ui'
-//
-// function init () {
-//   let app = document.querySelector('#app')
-//   return new GameUI(app)
-// }
-// document.addEventListener('DOMContentLoaded', init)
